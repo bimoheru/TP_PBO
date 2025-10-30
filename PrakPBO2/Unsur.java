ddc22fb28283c96130dc82f0c526a5ffc2e11343
Bimz_ <bimaheru0316@gmail.com>
2025-10-30 20:20:27 +0700

    Tugas 2 Praktikum PBO

---
+++ PrakPBO2/Unsur.java
@@ -0,0 +1,9 @@
+package tugasPrak1;
+
+public class Unsur {
+    String univ = "Universitas Suryakancana";
+    
+    void tampil(){
+        System.out.println(univ + "\nUniversitas pilihan di Cianjur\nKuliah di Fakultas Teknik\nJurusan Teknik Informatika");
+    }
+}
---
+++ PrakPBO2/sukaBelajar.java
@@ -0,0 +1,9 @@
+package tugasPrak1;
+
+public class sukaBelajar {
+    String matkul;
+    
+    void tampil(){
+        System.out.println("Saya suka " + matkul + "\nBelajar " + matkul + " asik\nYuk kita enjoy cihuuuyyyy");
+    }
+}
