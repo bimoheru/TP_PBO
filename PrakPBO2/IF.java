b25dd7088eaf6ff42bf92a832e907c3818480dca
Bimz_ <bimaheru0316@gmail.com>
2025-10-30 20:19:46 +0700

    Tugas 3 Praktikum PBO

---
+++ PrakPBO2/IF.java
@@ -0,0 +1,9 @@
+package tugasPrak1;
+
+public class IF {
+    String nama = "M Bimo Heru W";
+    
+    void tampil(){
+        System.out.println("Saya " + nama + ", adalah mahasiswa Fakultas Teknik Informatika\nJujur, amanah, dan informatikawan\nSenang belajar PBO");
+    }
+}
